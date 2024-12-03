5d5c77f830ffb9df38d3f3a56c9acc45123ce966
Satyamray21 <satyamray43@gmail.com>
2024-12-03 11:14:55 +0530

    added leetcode in footer

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from 'react';
+
 export default function About() {
     return (
         <div className="py-16 bg-white">
             <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
                 <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                     <div className="md:5/12 lg:w-5/12">
-                    <img
-                src="https://drive.google.com/uc?id=1c6_5J8NbPeX-JSdkZENhMDaIwB4-fyY5"
-                alt="Image from Google Drive"
-                style={{ maxWidth: "100%", height: "auto" }}
-            />
+                        <img 
+                            src="/images/myImage.jpg" 
+                            alt="My Image" 
+                            style={{ maxWidth: "100%", height: "auto" }}
+                        />
                     </div>
                     <div className="md:7/12 lg:w-6/12">
                         <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 export default function Footer() {
     return (
         <footer className="bg-white border-y">
@@ -24,10 +24,18 @@ export default function Footer() {
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link to="/Leetcode" className="hover:underline">
-                                        LeetCode
-                                    </Link>
+                                <NavLink
+                                    to="/Leetcode" 
+                                    className={({isActive}) =>
+                                    `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700":"text-grey-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                }>    
+                                    LeetCode
+                                    </NavLink>
                                 </li>
+                                
+                                
+                                   
+                                
                             </ul>
                         </div>
                         <div>
